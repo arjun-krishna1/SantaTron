489a2dfbf5777cd657f1eafec7c003bbb209a591
Arjun Krishna <arjunkrishna1306@gmail.com>
2021-12-24 12:12:56 -0500

    add initial GiftPool model

--- gift_coordinator/models.py
+++ gift_coordinator/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class GiftPool(models.Model):
+    recip_name = models.CharField(max_length=100)
+    creation_date = models.DateTimeField('date created', default = timezone.now())
+
