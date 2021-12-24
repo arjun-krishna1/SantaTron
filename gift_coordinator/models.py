f4c52ca3b20268971f5f4860806d9c191fcac832
Arjun Krishna <arjunkrishna1306@gmail.com>
2021-12-24 15:02:07 -0500

    add join or create new pool form

--- gift_coordinator/models.py
+++ gift_coordinator/models.py
@@ -3,17 +3,19 @@ from django.utils import timezone
 
 
 class GiftPool(models.Model):
-    recip_name = models.CharField(max_length=100)
+    recipient_name = models.CharField(max_length=100, default="")
     creation_date = models.DateTimeField('date created', default=timezone.now)
-    curr_val = models.DecimalField(decimal_places=2, max_digits=10)
-    search_query = models.CharField(max_length=200)
+    curr_val = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    search_query = models.CharField(max_length=200, default='')
 
 
 class Contributor(models.Model):
-    name = models.CharField(max_length=100)
-    amount_contributed = models.DecimalField(decimal_places=2, max_digits=8)
+    name = models.CharField(max_length=100, default="")
+    amount_contributed = models.DecimalField(decimal_places=2, max_digits=8, default = 0)
     gift_pool = models.ForeignKey(GiftPool, on_delete=models.CASCADE)
-    keyword = models.CharField(max_length=50)
+    keyword1 = models.CharField(max_length=50, default="")
+    keyword2 = models.CharField(max_length=50, default="")
+    keyword3 = models.CharField(max_length=50, default = "")
 
 # source query by filtering all contributor objects linked to pool, extracting keyword fields, and passing to model/API
 # for debug purposes, construct search query on each new contributor
--- gift_coordinator/views.py
+++ gift_coordinator/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+)
+
+from .models import (
+    GiftPool,
+    Contributor,
+)
+
+from .forms import CreatePoolForm
 
 def createPoolView(request):
+    if request.method == 'POST':
+        form = CreatePoolForm(request.POST)
+        if form.is_valid():
+
+            giftRecipName = form.cleaned_data.get('recipient_name').lower()
+            
+            pool = GiftPool.objects.filter(recipient_name=giftRecipName)
+            
+            newPoolVal = int(form.cleaned_data.get('amount'))
+            
+            if pool and pool.curr_val: # this is not the first person joining the gift pool
+                newPoolVal = pool.curr_val + newPoolVal
+
+            else:
+                pool = GiftPool(
+                    recipient_name = giftRecipName,
+                    curr_val = newPoolVal,
+                    )
+
+            # save this GiftPool
+            pool.save()
+
+            # create this contributor entry
+            contributor = Contributor(
+                name = form.cleaned_data.get('contributor_name'),
+                amount_contributed = form.cleaned_data.get('amount'),
+                gift_pool = pool,
+                keyword1 = form.cleaned_data.get('keyword1'),
+                keyword2 = form.cleaned_data.get('keyword2'),
+                keyword3 = form.cleaned_data.get('keyword3')
+            )
+
+            contributor.save()
+            return redirect(reverse('createPoolUrl'))
+
+
     context = {}
+    context['form'] = CreatePoolForm()
     return render(request, "createPoolTemplate.html", context)
   
