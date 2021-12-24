c52a0e9cc09bb36dac053cf9280a23a8dcc1c7d2
Joe <joedolina@gmail.com>
2021-12-24 12:37:15 -0500

    finish models

--- gift_coordinator/models.py
+++ gift_coordinator/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 
 class GiftPool(models.Model):
     recip_name = models.CharField(max_length=100)
-    creation_date = models.DateTimeField('date created', default=timezone.now())
+    creation_date = models.DateTimeField('date created', default=timezone.now)
     curr_val = models.DecimalField(decimal_places=2, max_digits=10)
     search_query = models.CharField(max_length=200)
 
